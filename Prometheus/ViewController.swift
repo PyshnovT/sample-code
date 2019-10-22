2497a6aaf497f19b1627d2405c4ef5b84d205dfa
Tim Pyshnov <pyshnovdev@gmail.com>
2019-10-22 10:36:39 +0300

    Initial Commit

---
+++ Prometheus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prometheus
+//
+//  Created by Tim Pyshnov on 22/10/2019.
+//  Copyright © 2019 Draewil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
